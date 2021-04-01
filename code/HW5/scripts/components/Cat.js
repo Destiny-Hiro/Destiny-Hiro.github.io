fdcb624ce086181013835005d4e8ee121e74d4f8
Destiny-Hiro <79959629+Destiny-Hiro@users.noreply.github.com>
2021-03-31 20:12:38 -0500

    dsfa
    sadf

--- code/HW5/scripts/components/Cat.js
+++
@@ -1,9 +0,0 @@
-const Cat = (catto) => ( 		//Function for HTML component
-	`
-	
-		<img class="animals" src="${catto.url}">
-	</div>
-	`
-) 								//Return the Cat url
-
-export default Cat; 			//Export Cat function
--- code/HW5/scripts/components/Dog.js
+++
@@ -1,9 +0,0 @@
-const Dog = (doggo) => ( 		//Function for HTML component
-	`
-	<div class="center">
-		<img class="animals" src="${doggo.message}">
-
-	`
-) 								//Return the Dog url
-
-export default Dog; 			//Export Dog function
