5a1ed8321723afce13a71c4851923e0a269ce603
Destiny-Hiro <79959629+Destiny-Hiro@users.noreply.github.com>
2021-03-31 20:13:25 -0500

    HW5 Final
    dffdg

---
+++ code/HW5/scripts/components/Choice.js
@@ -0,0 +1,13 @@
+const Choice = () =>(	
+	`<div class="center">
+		<div class="blankSpace"> </div>
+		<div class="blankSpace"> </div>
+		<div class="blankSpace"> </div>
+		<div class="blankSpace"> </div>
+		<div class="blankSpace"> </div>
+		<button class="big-button" onclick="checkAnswer('Dog')">Dog</button>	
+		<button class="big-button" onclick="checkAnswer('Cat')">Cat</button>
+	</div>`
+)
+
+export default Choice; //Export Choice function
---
+++ code/HW5/scripts/components/Dog.js
@@ -0,0 +1,9 @@
+const Dog = (doggo) => ( 		//Function for HTML component
+	`
+	<div class="center">
+		<img class="animals" src="${doggo.message}">
+
+	`
+) 								//Return the Dog url
+
+export default Dog; 			//Export Dog function
